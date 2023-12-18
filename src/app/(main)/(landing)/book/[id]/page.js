a55da94c531d3a0a7f5cf957085d88fbfd2ba483
duongthanhnam24 <thanh.nam.duong@draphony.com>
2023-12-18 22:59:51 +0700

    new

--- src/app/(main)/(landing)/book/[id]/page.js
+++ src/app/(main)/(landing)/book/[id]/page.js
@@ -1,5 +1,10 @@
 "use client";
 
+import Table from "@/components/table/table";
+import Td from "@/components/td/td";
+import Th from "@/components/th/th";
+import Thead from "@/components/thead/thead";
+import Tr from "@/components/tr/tr";
 import { Button } from "@/components/ui/button";
 import { getOrderUser } from "@/service/product";
 import { useEffect, useState } from "react";
@@ -35,50 +40,50 @@ function Book({ params }) {
     };
     return (
         <section>
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-auto min-h-[500px]">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3 ">
+            <Table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-auto min-h-[500px]">
+                <Thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <Tr>
+                        <Th scope="col" className="px-6 py-3 ">
                             Number
-                        </th>
-                        <th scope="col" className="px-6 py-3 ">
+                        </Th>
+                        <Th scope="col" className="px-6 py-3 ">
                             Tên sách
-                        </th>
-                        <th scope="col" className="px-6 py-3">
+                        </Th>
+                        <Th scope="col" className="px-6 py-3">
                             Tác giả
-                        </th>
+                        </Th>
 
-                        <th scope="col" className="px-6 py-3">
+                        <Th scope="col" className="px-6 py-3">
                             Thể loại
-                        </th>
-                        <th scope="col" className="px-6 py-3">
+                        </Th>
+                        <Th scope="col" className="px-6 py-3">
                             Thời gian mượn
-                        </th>
-                        <th scope="col" colSpan={2} className="px-6 py-3 ">
+                        </Th>
+                        <Th scope="col" colSpan={2} className="px-6 py-3 ">
                             Action
-                        </th>
-                    </tr>
-                </thead>
+                        </Th>
+                    </Tr>
+                </Thead>
                 <tbody>
                     {data?.orderItems?.map((item, i) => {
                         return (
-                            <tr
+                            <Tr
                                 className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-cyan-200 dark:hover:bg-gray-600 cursor-pointer "
                                 key={i}
                             >
-                                <td className="px-6 py-4">{i + 1}</td>
+                                <Td className="px-6 py-4">{i + 1}</Td>
                                 <th
                                     scope="row"
                                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                                 >
                                     {item.name}
                                 </th>
-                                <td className="px-6 py-4">{item.author}</td>
+                                <Td className="px-6 py-4">{item.author}</Td>
 
-                                <td className="px-6 py-4">{item.type}</td>
-                                <td className="px-6 py-4">{item.dateBorrow}</td>
+                                <Td className="px-6 py-4">{item.type}</Td>
+                                <Td className="px-6 py-4">{item.dateBorrow}</Td>
 
-                                <td className="py-4 ">
+                                <Td className="py-4 ">
                                     <Button
                                         className="px-4 py-2 text-white rounded-xl bg-red-500"
                                         onClick={() => handleBook(data._id, item.product)}
@@ -86,12 +91,12 @@ function Book({ params }) {
                                         {" "}
                                         Trả sách
                                     </Button>
-                                </td>
-                            </tr>
+                                </Td>
+                            </Tr>
                         );
                     })}
                 </tbody>
-            </table>
+            </Table>
         </section>
     );
 }
